dda3390ca756deec5aeb6be3c29470b883d012d2
gabrielosorioz <gabrielosorio013@gmail.com>
2024-01-28 08:51:26 -0300

    style(script.js): Adicionar Funcionalidade de Alternância ao Botão 'Adicionar aos Favoritos'

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,18 @@ $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
  const /** {NodeElement} */ $header = document.querySelector("[data-header]");
 
  window.addEventListener("scroll" , e => {
-    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 
  })
+
+ /**
+ * add to favorite button toggle
+ */
+
+ const /** {NodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach( $toggleBtn => {
+   $toggleBtn.addEventListener("click", () => {
+      $toggleBtn.classList.toggle("active")
+   })
+})
